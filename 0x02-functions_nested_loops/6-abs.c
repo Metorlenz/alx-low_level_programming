ec34011101ac8659a48b80e942977883e7687a71
root <ntenkamtefonyuy@yahoo.fr>
2022-03-16 14:26:32 -0700

    absolute interger

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,6 +3,7 @@
 /**
  * _abs - computes the absolute value for an interger
  * @n: interger tested
+ * Description: This function returns absolute value of a number
  * Return 0 or 1
  */
 int _abs(int n)
